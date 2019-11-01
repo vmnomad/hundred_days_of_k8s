4a971c51040e430eaa19419c2a97893ea99f102a
vmnomad <akopbayev@icloud.com>
2019-11-01 20:36:11 +1100

    Rework navigration bar
    Figured out how to split nav bar elements into different parts of
    the page
    
    Added fav icon
    
    Changed navbar to static top to always keep it on the screen.
    Added padding to the top of the screen as static top navbar
    overlaps with the rest of the page.

--- website/views.py
+++ website/views.py
@@ -4,16 +4,22 @@ import markdown
 
 def home(request):
     context = {
-        'content' : 'Default Content'
+        'content' : 'This text is built dynamiccaly by Django View function'
     }
+    print("Path", request.path)
     return render(request, 'index.html', context=context)
     #return HttpResponse('Hello, world!')
 
 def about(request):
+
     return HttpResponse('About Me!')
 
 def three_w(request):
-    return HttpResponse('three W!')
+    context = {
+        'content' : 'This is 3Ws page'
+    }
+    print("Path", request.path)
+    return render(request, '3w.html', context=context)
 
 def rules(request):
     return HttpResponse('rules!')
