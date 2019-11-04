7b2fde3f9858ffa2309a4c2007e8e2877b9b4a3e
vmnomad <akopbayev@icloud.com>
2019-11-05 07:55:22 +1100

    Fix test error
    Removed error that was used to test CircleCI pipeline

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path(r'', RedirectView.as_view(url='/home/')), # redirect to home url to let view get the md file name from url
-    path('hom/', views.default, name='home'),
+    path('home/', views.default, name='home'),
     path('about/', views.default, name='about'),
     path('3w/', views.default, name='three_w'),
     path('rules/', views.default, name='rules'),
