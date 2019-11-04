34486f64aebd0100375456a8c31d66863f557763
vmnomad <akopbayev@icloud.com>
2019-11-05 08:19:16 +1100

    Productionise Django
    installed gunicorn
    Added dj-static for serving static files
    updated requirements.txt

--- hundred_days_of_k8s/wsgi.py
+++ hundred_days_of_k8s/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hundred_days_of_k8s.settings')
 
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
