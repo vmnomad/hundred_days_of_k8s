78dbf49503cedc8834b240bd3cabe00285ea79c7
vmnomad <akopbayev@icloud.com>
2019-11-05 10:29:31 +1100

    Fix website for pythonanywhere
    Fixed path generation in utils.py
    Fixed Active element for 3Ws element in navbar

--- website/utils.py
+++ website/utils.py
@@ -1,9 +1,10 @@
 import markdown
 import os
-
+module_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(module_path)
 def convert_md_html(file_name):
-    cd = os.getcwd()
-    folder = os.path.join(cd, 'website/content')
+
+    folder = os.path.join(dir_path, 'content')
     md_file =os.path.join( folder, file_name)
     input_file = open(md_file, mode="r", encoding="utf-8")
     text = input_file.read()
