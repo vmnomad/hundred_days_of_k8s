18e36ad073c14f2fa54baa01dabc4096781ecff9
vmnomad <akopbayev@icloud.com>
2019-11-10 15:23:31 +1100

    update SSL cert

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ cd /hundred_days_of_k8s
 #prepare nging config
 cp website.conf /etc/nginx/conf.d/
 cp proxy_params /etc/nginx/
-cp certificates/domain-crt.crt /etc/nginx/
-cp certificates/domain-key.key /etc/nginx/
+cp certificates/100daysofk8s.crt/etc/nginx/
+cp certificates/100daysofk8s.key /etc/nginx/
 rm /etc/nginx/conf.d/default.conf
 mkdir -p /run/nginx
 
